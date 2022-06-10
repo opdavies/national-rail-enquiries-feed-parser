13d023b874c96c29fdea90b5180a25b3210f2bd5
Oliver Davies <oliver.davies@tfw.wales>
2022-06-10 19:18:11 +0100

    Revert "refactor: remove StationSerializer"
    This reverts commit 762a73b88a71e9cd0524c99a512314dba1b8b186.

--- src/Parser/AbstractStationParser.php
+++ src/Parser/AbstractStationParser.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Opdavies\NationalRailEnquriesFeedParser\Parser;
 
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
-use Symfony\Component\Serializer\Serializer;
+use Opdavies\NationalRailEnquriesFeedParser\Serializer\StationSerializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 abstract class AbstractStationParser implements StationsFeedParser
@@ -17,9 +13,6 @@ abstract class AbstractStationParser implements StationsFeedParser
 
     public function __construct()
     {
-        $this->serializer = new Serializer(
-            [new ObjectNormalizer(), new PropertyNormalizer(), new ArrayDenormalizer()],
-            [new JsonEncoder()]
-        );
+        $this->serializer = new StationSerializer();
     }
 }
