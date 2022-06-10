762a73b88a71e9cd0524c99a512314dba1b8b186
Oliver Davies <oliver.davies@tfw.wales>
2022-06-10 15:46:13 +0100

    refactor: remove StationSerializer
    This is no longer needed as nothing is being overridden when
    deserializing.

--- src/Parser/AbstractStationParser.php
+++ src/Parser/AbstractStationParser.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Opdavies\NationalRailEnquriesFeedParser\Parser;
 
-use Opdavies\NationalRailEnquriesFeedParser\Serializer\StationSerializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 abstract class AbstractStationParser implements StationsFeedParser
@@ -13,6 +17,9 @@ abstract class AbstractStationParser implements StationsFeedParser
 
     public function __construct()
     {
-        $this->serializer = new StationSerializer();
+        $this->serializer = new Serializer(
+            [new ObjectNormalizer(), new PropertyNormalizer(), new ArrayDenormalizer()],
+            [new JsonEncoder()]
+        );
     }
 }
