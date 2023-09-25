3baa44c096ce5a0690bc234602a4800bced68a5f
Oliver Davies <oliver.davies@tfw.wales>
2023-09-25 11:57:41 +0100

    test(json-parser): add JSON tests for closing
    ...times

--- tests/Parser/StationsJsonFeedParserTest.php
+++ tests/Parser/StationsJsonFeedParserTest.php
@@ -93,3 +93,57 @@ it("returns the postcode for a station", function () {
 
     expect($station->getPostcode())->toBe('CF83 1AQ');
 });
+
+it('returns the ticket office closing time for a station where days are set as MondayToFriday', function () {
+    $data = file_get_contents(__DIR__.'/../stubs/NWP.json');
+
+    $station = $this->parser->parseStation($data);
+
+    expect($station->getClosingTime(day: 'Monday'))->toBe('20:00');
+    expect($station->getClosingTime(day: 'Tuesday'))->toBe('20:00');
+    expect($station->getClosingTime(day: 'Wednesday'))->toBe('20:00');
+    expect($station->getClosingTime(day: 'Thursday'))->toBe('20:00');
+    expect($station->getClosingTime(day: 'Friday'))->toBe('20:00');
+
+    expect($station->getClosingTime(day: 'Saturday'))->toBe('20:30');
+    expect($station->getClosingTime(day: 'Sunday'))->toBe('19:45');
+});
+
+it('returns the ticket office closing time for a station where days are defined as different days', function () {
+    $json = file_get_contents(__DIR__.'/../stubs/TRF.json');
+
+    $station = $this->parser->parseStation($json);
+
+    expect($station->getClosingTime(day: 'Monday'))->toBe('13:00');
+    expect($station->getClosingTime(day: 'Tuesday'))->toBe('13:00');
+    expect($station->getClosingTime(day: 'Wednesday'))->toBe('13:00');
+    expect($station->getClosingTime(day: 'Thursday'))->toBe('13:00');
+    expect($station->getClosingTime(day: 'Friday'))->toBe('13:00');
+    expect($station->getClosingTime(day: 'Saturday'))->toBe('13:00');
+
+    expect($station->getClosingTime(day: 'Sunday'))->toBeNull();
+});
+
+it('returns null if a station does not have a ticket office', function() {
+    $data = <<<EOF
+      {
+        "CrsCode": "TRE",
+        "Name": "Trefforest Estate",
+        "Fares": {
+        "TicketOffice": {
+          "Available": false
+        }
+      }
+    }
+    EOF;
+
+    $station = $this->parser->parseStation($data);
+
+    expect($station->getClosingTime(day: 'Monday'))->toBeNull();
+    expect($station->getClosingTime(day: 'Tuesday'))->toBeNull();
+    expect($station->getClosingTime(day: 'Wednesday'))->toBeNull();
+    expect($station->getClosingTime(day: 'Thursday'))->toBeNull();
+    expect($station->getClosingTime(day: 'Friday'))->toBeNull();
+    expect($station->getClosingTime(day: 'Saturday'))->toBeNull();
+    expect($station->getClosingTime(day: 'Sunday'))->toBeNull();
+});
